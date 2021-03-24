0b68d223308c490c9039d562f33d5e891af3e3ae
Clayman <Clayman@DESKTOP-DM8E77U>
2021-03-24 17:48:39 +0200

    Constructors.

--- Intro/Intro/main.cpp
+++ Intro/Intro/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include<iostream>
 using namespace std;
 
+#define tab "\t"
+
 //Создание структуры:
 class Point
 {
@@ -8,11 +10,11 @@ class Point
 	double x;//Координата по X
 	double y;//Координата по Y
 public:
-	double get_x()
+	double get_x()const
 	{
 		return x;
 	}
-	double get_y()
+	double get_y()const
 	{
 		return y;
 	}
@@ -25,20 +27,54 @@ public:
 		this->y = y;
 	}
 	//			Constructors:
-	Point()
+	/*Point()
 	{
-		x = y = 0;
+		//Конструктор по умолчанию создает точку в начале коорлинат
+		x = y = int();	//Якобы вызываем констуктор по умолчанию для 'int', и он возвращает значение по умолчанию
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
 	Point(double x)
 	{
+		//Конструктор с одним параметром создает точку на прямой.
 		this->x = x;
 		this->y = 0;
+		cout << "SingleArgumentConstructor:" << this << endl;
+	}
+	Point(double x, double y)
+	{
+		//Конструктор с параметрами создает точку на плоскости
+		this->x = x;
+		this->y = y;
 		cout << "Constructor:\t\t" << this << endl;
+	}*/
+	Point(double x = 0, double y = 0)
+	{
+		//Этот кнструктор с параметрами может быть вызван 
+		//-без парамтеров;
+		//-с одним параметром;
+		//-с двумя параметрами;
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		//other - это другой объект, копию которого мы создаем
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
 	}
 	~Point()
 	{
 		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//			Operators:
+	void operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
 	}
 
 	//				Methods:
@@ -46,16 +82,31 @@ public:
 	{
 		cout << "X = " << x << "\tY = " << y << endl;
 	}
+	double distance(const Point& other)const
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = other.y - this->y;
+		double distance = sqrt(x_distance*x_distance + y_distance * y_distance);//sqrt - Square Root (Квадратный корень)
+		return distance;
+	}
 };
 //Создавая структуру или класс, мы создаем новый тип данных, 
 //а объявляя объекты этого класса или структуры мы создаем переменные нашего типа.
-
 //			КЛАСС - ЭТО ТИП ДАННЫХ
 //		СТРУКТУРА - ЭТО ТИП ДАННЫХ
 //		ОБЪЕКТ - ЭТО САМАЯ ОБЫЧНАЯ ПЕРЕМЕННАЯ.
 
+double distance(const Point& A, const Point& B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance*x_distance + y_distance * y_distance);
+	return distance;
+}
+
 //#define INTRO
-#define CONSTRUCTORS
+//#define CONSTRUCTORS
+#define DISTANCE
 
 void main()
 {
@@ -76,11 +127,39 @@ void main()
 	Point A;//Default constructor
 	cout << A.get_x() << "\t" << A.get_y() << endl;
 
-	Point B = 5;
+	Point B = 5;//Single-Argument constructor
 	B.print();
+
+	Point C(8);	//Single-Argument constructor
+	C.print();
+
+	Point D{ 12 };//Single-Argument constructor
+	D.print();
+
+	int a(2);
+	int b{ 3 };
+	cout << a << tab << b << endl;
+
+	Point E(5, 3);//Parametrized constructor
+	E.print();
+
+	Point F = E;//CopyConstructor. Здесь мы создаем объект, и инициализируем его другим объектом
+	Point G;	//DefaultConstructor
+	G = D;	//CopyAssignment - operator=
+	G.print();
 #endif // CONSTRUCTORS
 
+	Point A(2, 3);
+	Point B(4, 5);
+	A.print();
+	B.print();
+	cout << A.distance(B) << endl;
+	cout << distance(A, B) << endl;
+	int();
+	A.print();
+	B.print();
 }
+
 /*
 -------------------------------------
 .  - Оператор прямого доступа (Point operator) используется для доступа к полям объекта по имени объекта
@@ -93,7 +172,7 @@ this - это указатель на объект, для которого вы
 -------------------------------------
 1. Encapsulation - это сокрытие определенной части класса от внешнего мира;
 	Модификаторы доступа:
-		private: закрытые поля (члены) класса, доступные только изнутри класса 
+		private: закрытые поля (члены) класса, доступные только изнутри класса
 				 и недоступны за его пределами.Здесь обязательно должны находиться ПЕРЕМЕННЫЕ ЧЛЕНЫ КЛАССА!!!
 		public:	 открытые поля, доступные из любого места программы, где доступен объект.
 				 В public-секции обычно размещают методы.
@@ -108,7 +187,7 @@ this - это указатель на объект, для которого вы
 
 /*
 -------------------------------------
-Constructor - это метод, который создает объект. 
+Constructor - это метод, который создает объект.
 			  Выделяет память под объект и инициализирует его поля при создании.
 Destructor  - это метод, который уничтожает объект по истечении его времени жизни.
 ~
@@ -125,7 +204,7 @@ class Class
 };
 for(Point A = 0; A < 10; A++)
 {
-	
+
 }
 if(четотам)
 {
