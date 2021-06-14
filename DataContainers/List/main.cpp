7b7230871ca3cf616b219092534d07578843942e
Clayman <Clayman@DESKTOP-DM8E77U>
2021-06-14 18:34:41 +0300

    Operator plus DONE.

--- DataContainers/List/main.cpp
+++ DataContainers/List/main.cpp
@@ -394,9 +394,17 @@ public:
 	}
 };
 
+List operator+(const List& left, const List& right)
+{
+	List cat = left;
+	for (int i : right)cat.push_back(i);
+	return cat;
+}
+
 //#define BASE_CHECK
 //#define SIZE_CONSTRUCTOR_AND_INDEX_OPERATOR
 //#define ITERATORS_CHECK
+//#define COPY_METHODS_CHECK
 
 void main()
 {
@@ -452,10 +460,17 @@ void main()
 	cout << endl;
 #endif // ITERATORS_CHECK
 
+#ifdef COPY_METHODS_CHECK
 	List list1 = { 3,5,8,13,21 };
 	list1.print();
 	//List list2 = list1;	//CopyConstructor
 	List list2;
 	list2 = list1;	//CopyAssignment
 	list2.print();
+#endif // DEBUG
+
+	List list1 = { 3,5,8,13,21 };
+	List list2 = { 34,55,89 };
+	List list3 = list1 + list2;
+	list3.print();
 }
