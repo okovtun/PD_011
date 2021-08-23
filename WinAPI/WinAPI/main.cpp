100916c3ac4041febd0bd71e314b2a945e04f4d6
Clayman <Clayman@DESKTOP-DM8E77U>
2021-08-23 18:18:43 +0300

    Work with text DONE.

--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -1,6 +1,15 @@
 #include<Windows.h>
 #include"resource.h"
 
+HWND hEdit1;
+HWND hEdit2;
+
+CONST UINT TEXT_SIZE = 256;
+
+//CHAR sz_text_1[TEXT_SIZE] = { "Какойто текст..." };
+//CHAR sz_text_2[TEXT_SIZE] = {};
+CHAR sz_buffer[TEXT_SIZE] = {};
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
@@ -18,13 +27,30 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON2));
 		SendMessage(hwnd, WM_SETICON, 1, (LPARAM)hIcon);
+
+		hEdit1 = GetDlgItem(hwnd, IDC_EDIT1);
+		hEdit2 = GetDlgItem(hwnd, IDC_EDIT2);
+		SendMessage(hEdit1, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		//SetFocus(hEdit1);
 	}
 	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
-		case IDOK: MessageBox(NULL, "Была нажата кнопка OK", "Info", MB_ICONINFORMATION | MB_OK); break;
+		case IDOK:
+		{
+			SendMessage(hEdit1, WM_GETTEXT, (WPARAM)TEXT_SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEdit2, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			//HWND hStatic2 = GetDlgItem(hwnd, IDC_STATIC2);
+			//SendMessage(hStatic2, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			
+			//SendMessage(GetDlgItem(hwnd, IDC_STATIC2), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SetDlgItemText(hwnd, IDC_STATIC2, sz_buffer);
+
+			//MessageBox(NULL, "Была нажата кнопка OK", "Info", MB_ICONINFORMATION | MB_OK); 
+		}
+		break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
 	}
--- WinAPI/WinAPI/resource.h
+++ WinAPI/WinAPI/resource.h
@@ -5,6 +5,9 @@
 #define IDD_DIALOG1                     101
 #define IDI_ICON1                       103
 #define IDI_ICON2                       104
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_STATIC2                     1004
 
 // Next default values for new objects
 // 
@@ -12,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
