fc35c7fe1fe8e7bde175cc1c36a2835248a422da
Clayman <Clayman@DESKTOP-DM8E77U>
2021-08-27 18:34:13 +0300

    Exam

--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -1,10 +1,27 @@
 #include<Windows.h>
 #include"resource.h"
 
+struct Parent
+{
+	int a;
+};
+
+struct Child :public Parent
+{
+
+};
+
+CONST CHAR* cb_content[] =
+{
+	"This","is", "my", "first","Combo","Box"
+};
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
+	Child child;
+	child.a = 2;
 	//MessageBox(NULL, "Это мое первое окно, созданное в WinAPI", "Info MessageBox", MB_ICONWARNING | MB_YESNOCANCEL);
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProc, 0);
 	return 0;
@@ -18,13 +35,30 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON2));
 		SendMessage(hwnd, WM_SETICON, 1, (LPARAM)hIcon);
+
+		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+		for (int i = 0; i < sizeof(cb_content) / sizeof(cb_content[0]); i++)
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)cb_content[i]);
+		SendMessage(hCombo, CB_SETCURSEL, 2, 0);
 	}
 	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
-		case IDOK: MessageBox(NULL, "Была нажата кнопка OK", "Info", MB_ICONINFORMATION | MB_OK); break;
+		case IDOK:
+		{
+			//MessageBox(NULL, "Была нажата кнопка OK", "Info", MB_ICONINFORMATION | MB_OK); 
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			CHAR sz_buffer[256]{};
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+			CHAR sz_msg[256]{};
+			strcpy(sz_msg, "Вы выбрали пункт:");
+			strcat(sz_msg, sz_buffer);
+			MessageBox(hwnd, sz_msg, "Info", MB_ICONINFORMATION | MB_OK);
+		}
+		break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
 	}
--- WinAPI/WinAPI/resource.h
+++ WinAPI/WinAPI/resource.h
@@ -5,6 +5,7 @@
 #define IDD_DIALOG1                     101
 #define IDI_ICON1                       103
 #define IDI_ICON2                       104
+#define IDC_COMBO1                      1001
 
 // Next default values for new objects
 // 
@@ -12,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
