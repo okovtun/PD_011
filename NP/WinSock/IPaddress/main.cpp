941d87bb995912e1a6ba980900a956bb0f0ee4bb
Clayman <Clayman@DESKTOP-DM8E77U>
2021-12-13 17:14:38 +0200

    GetAdapterAddress DONE.

--- NP/WinSock/IPaddress/main.cpp
+++ NP/WinSock/IPaddress/main.cpp
@@ -1,3 +1,8 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#ifdef UNICODE
+#undef UNICODE
+#endif
+
 #include<iostream>
 #include<WinSock2.h>
 #include<iphlpapi.h>
@@ -41,8 +46,62 @@ void main()
 		return;
 	}
 
-	//Второй вызов GetAdapterAddress() уже заолняет структуру pAddress всеми данными об интерфейсах.
-	GetAdaptersAddresses(AF_INET, NULL, NULL, pAddress, &outBufLen);
+	//Второй вызов GetAdapterAddress() уже заполняет структуру pAddress всеми данными об интерфейсах.
+	DWORD dwRetVal = GetAdaptersAddresses(AF_INET, NULL, NULL, pAddress, &outBufLen);
+
+	if (dwRetVal == NO_ERROR)
+	{
+		for (PIP_ADAPTER_ADDRESSES pCurAddress = pAddress; pCurAddress; pCurAddress = pCurAddress->Next)
+		{
+			cout << "Interface Index:\t" << pCurAddress->IfIndex << endl;
+			cout << "Interface name:\t" << pCurAddress->AdapterName << endl;
+			wcout << "Description:\t" << pCurAddress->Description << endl;
+
+			DWORD dwAddressStringLength = 256;	//Размер строки, содержащей IP-адрес
+			//Unicast	- уникальный адрес, который назначается интерфейсу (сетевая карта)
+			//Broadcast	- широковещательный адрес, НЕ может быть назначен интерфейсу, 
+			//			  вкладывается в широковещательный IP-пакеты.
+			//Multicast - групповые адреса, назначаются группе узлов, как правило протоколом высшего уровня.
+			//ncpa.cpl
+
+			//		Проход по списку Unicast-адресов текущего адаптера (pCurAddress):
+			for (PIP_ADAPTER_UNICAST_ADDRESS_LH pUnicast = pCurAddress->FirstUnicastAddress; pUnicast; pUnicast = pUnicast->Next)
+			{
+				//Выделяем память для строки, в которой будет хранится полученный IP-адрес.
+				LPSTR lpszAddressString = (LPSTR)HeapAlloc(GetProcessHeap(), 0, dwAddressStringLength);
+				//Представляем адрес в точечно-десятичной нотации:
+				WSAAddressToString(pUnicast->Address.lpSockaddr, pUnicast->Address.iSockaddrLength, NULL, lpszAddressString, &dwAddressStringLength);
+				//Буква W в имени функции, или типа данных означает WIDE_CHAR (w_char_t) - символы в кодировке Unicode
+
+				/*cout << "\tIP address:\t" << lpszAddressString;
+				wcout << "/" << pUnicast->OnLinkPrefixLength << endl;*/
+				//wcout << "\tSubnet prefix:\t" << pUnicast->OnLinkPrefixLength << endl;	
+				printf("\tIP address:\t%s/%d\n", lpszAddressString, pUnicast->OnLinkPrefixLength);
+				//%s - string
+				//TODO:
+				/*ULONG lMask = 0;
+				if (ConvertLengthToIpv4Mask(pUnicast->OnLinkPrefixLength, &lMask) == NO_ERROR)
+				{
+					cout << "\tSubnet mask: " << lMask << endl;
+				}*/
+
+				HeapFree(GetProcessHeap(), 0, lpszAddressString);
+			}
+
+			if (pCurAddress->PhysicalAddressLength)
+			{
+				cout << "Phisical address (MAC): ";
+				for (int i = 0; i < pCurAddress->PhysicalAddressLength; i++)
+				{
+					cout.width(2);	//Задает ширину поля для вывода
+					cout.fill('0');	//Заполняет пустое место в выделенном поле заданным символом
+					cout << hex << (int)pCurAddress->PhysicalAddress[i];
+					if(i!=pCurAddress->PhysicalAddressLength-1)cout << ":";
+				}
+			}
+			cout << "\n----------------------------------------------------------\n";
+		}
+	}
 
 	HeapFree(GetProcessHeap(), 0, pAddress);
 
